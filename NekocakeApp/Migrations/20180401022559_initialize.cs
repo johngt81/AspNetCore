aea77b7e5a50a725bac4a2874943330dba4929e3
bgaona <bgaona@CTXDEV045>
2018-04-01 03:17:38 +0000

    Add migrations

---
+++ NekocakeApp/Migrations/20180401022559_initialize.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace NekocakeApp.Migrations
+{
+    public partial class initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                schema: "dbo",
+                columns: table => new
+                {
+                    FeedbackId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ContactMe = table.Column<bool>(nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false),
+                    Message = table.Column<string>(maxLength: 500, nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.FeedbackId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pie",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AllergyInformation = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: false),
+                    ImageThumbnailUrl = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    InStock = table.Column<bool>(nullable: false),
+                    IsPieOfTheWeek = table.Column<bool>(nullable: false),
+                    LongDescription = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ShortDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pie", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pie_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pie_CategoryId",
+                schema: "dbo",
+                table: "Pie",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedback",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Pie",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
--- NekocakeApp/Program.cs
+++ NekocakeApp/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NekocakeApp.Models;
 
 namespace NekocakeApp
 {
@@ -14,7 +16,14 @@ namespace NekocakeApp
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<AppDBContext>();
+                DBInitializer.Seed(dbContext);
+            }
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
